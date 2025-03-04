b997fa479824b44e0ec4b00ae0c295e094f2180a
thelo67 <tuomo.helo@turkuamk.fi>
2025-03-04 22:31:20 +0200

    Fix the error

--- Workout.js
+++ Workout.js
@@ -3,9 +3,9 @@ import { logger } from './logger.js';
 
 
 const createWorkout = username => {
-//    if (typeof username !== 'string' || username.length < 4) {
-//        throw new Error("the parameter username is not valid");
-//    }
+    if (typeof username !== 'string' || username.length < 4) {
+        throw new Error("the parameter username is not valid");
+    }
     return db('workouts').insert({ username });
 };
 
